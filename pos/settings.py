c1ce772cb198d9d9eb1b8f65756cd642ddc54772
Chaser-254 <esibitaremmanuel316@gmail.com>
2025-03-16 00:37:17 +0300

    Initial commit

--- pos/settings.py
+++ pos/settings.py
@@ -83,10 +83,9 @@ WSGI_APPLICATION = 'pos.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL','sqlite://db.sqlite3')
+    )
 }
 
 
